d3092647b3ee71d2a0300376802db649c6c6905d
Gabriel <victorgabriel1730@gmail.com>
2024-10-08 11:51:31 -0300

    fix: add from new args from connect event

--- src/server.py
+++ src/server.py
@@ -1,23 +1,28 @@
 from aiohttp import web
 import socketio
-
-
+from card_transaction import transaction
 sio = socketio.AsyncServer()
 
 app = web.Application()
 sio.attach(app)
-
-
 @sio.event
-
-def connect(sid):
+def connect(sid, environ, auth):
     print('connect', sid)
 
+@sio.event
+def proccess_data(sid, data):
+    print('Iniciando DPOS')
+    return transaction(data)
+
+@sio.event
+def disconnect(sid):
+    print('encerrando a sessao...')
+
 async def index(req):
-    with open('src/index.html') as f:
+    with open('index.html') as f:
         return web.Response(text = f.read(), content_type = 'text/html')
 
-app.router.add_static('/static', 'static')
+#app.router.add_static('/static', 'static')
 app.router.add_get('/', index)
 
 
